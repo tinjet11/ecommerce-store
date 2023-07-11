0187354d0426eed4e5a19af24e294ea922227f2c
tinjet_11 <tinjet1016@gmail.com>
2023-07-11 12:31:00 +0800

    adding addtocart in info

--- components/info.tsx
+++ components/info.tsx
@@ -3,12 +3,19 @@ import React from 'react'
 import Currency from './ui/currency';
 import Button from './ui/button';
 import { ShoppingCart } from 'lucide-react';
+import useCart from '@/hooks/use-cart';
 
 interface InfoProps {
     data: Product;
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
+    const cart = useCart();
+
+    const onAddToCart = ()=>{
+        cart.addItem(data);
+    }
+    
     return (
         <div>
             <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -32,7 +39,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
             </div>
             <div className="mt-10 flex items-center gap-x-3">
                 {/*      <Button onClick={onAddToCart} className="flex items-center gap-x-2"> */}
-                <Button onClick={() => { }} className="flex items-center gap-x-2">
+                <Button onClick={onAddToCart} className="flex items-center gap-x-2">
                     Add To Cart
                   <ShoppingCart size={20} />
                 </Button>
